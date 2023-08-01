2bbd2f36d866b3d57800ae4e2b1940c0ecf94bb6
Kamilo Vasquez <killo.blanco@gmail.com>
2023-08-01 09:52:51 -0500

    🚧 Layout

---
+++ src/components/logo.tsx
@@ -0,0 +1,9 @@
+import { type FC } from 'react'
+
+export const Logo: FC = () => (
+  <img
+    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert aspect-auto h-6"
+    src="/next.svg"
+    alt="Next.js Logo"
+  />
+)
---
+++ src/components/top-bar/index.tsx
@@ -0,0 +1,10 @@
+import { type FC } from 'react'
+import { Logo } from '../logo'
+
+export const TopBar: FC = () => (
+  <section
+    className="py-4 px-8 flex"
+  >
+    <Logo />
+  </section>
+)
